5d031d7abe72770940cd3bb0fc888fd295dd52fa
fakulp <agus.lp.md@gmail.com>
2024-12-06 14:53:28 -0300

    calculadora de promedios

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-const iva = 1.21;
+let idActual = prompt("ingrese un ID ");
+var stop = false;
 
-function calculo (importe) {
-    if (parseFloat(importe)){
-        let resultado = importe*iva;
-        alert("el imporet mas iva "+ resultado)
-    }
-}
 
-function precioFinal (){
-    let precio = parseFloat (prompt("ingrese valor del producto: "))
-    calculo(precio)
+while (!stop){
+    nota=0;
+    total=0;
+    promedio=0;
+
+        for (let i=1; i <= 5; i++){
+            let nota = parseFloat(prompt("Nota "));
+            if ((isNaN(nota)) || (nota <= 1)) {
+                nota = 1;
+            };
+            total= total + nota;
+        }
+
+    promedio =  parseFloat(total / 5);
+    alert(promedio);
+
+    let idActual = prompt("Nuevo un ID ");
+    if ((idActual == null) ) {
+    stop = true
+    };
+
 }
