a7b4d6023985164d40e27b6bdbff00e6391fcdb7
fakulp <agus.lp.md@gmail.com>
2024-12-23 15:21:09 -0300

    array

--- js/main.js
+++ js/main.js
@@ -1,26 +1,65 @@
-let idActual = prompt("ingrese un ID ");
+idActual = prompt("ingrese un ID ");
 var stop = false;
+
+
+const estudiante = function (idEstudiante, promedioEstudiante){
+    this.idEstudiante = idEstudiante;
+    this.promedioEstudiante = promedioEstudiante;
+}
+
+
+let vectorAlumno = [0]
+
+
+function agregarAlumno () {
+    let idEstudiante = idEstudiante;
+    let promedio = promedio;
+
+    let estudiante = new estudiante (idEstudiante, promedioEstudiante);
+    vectorAlumno.push(estudiante)
+}
+
+
+
+
+
+function sumaDeNotas () {
+    for (let i=1; i <= 5; i++){
+        let nota = parseFloat(prompt("Nota "));
+        if ((isNaN(nota)) || (nota <= 1)) {
+            nota = 1;
+        };
+        total= total + nota;
+    }
+}
+
 
 
 while (!stop){
     nota=0;
     total=0;
-    promedio=0;
+    promedioActual=0;
 
-        for (let i=1; i <= 5; i++){
-            let nota = parseFloat(prompt("Nota "));
-            if ((isNaN(nota)) || (nota <= 1)) {
-                nota = 1;
-            };
-            total= total + nota;
-        }
+    sumaDeNotas (nota, total);
 
-    promedio =  parseFloat(total / 5);
+    promedioActual =  parseFloat(total / 5);
     alert(promedio);
 
+
+    agregarAlumno(idActual, promedio);
+
+
+
+
+
     let idActual = prompt("Nuevo un ID ");
-    if ((idActual == null) ) {
+    if ((idActual == null) || (idActual =="")) {
     stop = true
     };
 
+
 }
+
+
+
+
